c0be5600832b9d14bafe9183186355cbaceb1dc5
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-10 20:55:33 +0800

    Create Main.java

---
+++ Latihan5/src/Latihan5/Main.java
@@ -0,0 +1,23 @@
+package Latihan5;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class Main {
+      int umur;
+    int PanjangPohon;
+    String JenisPohon;
+    
+    void info(){
+        System.out.println("Sebutkan Jenis Pohon : "+ JenisPohon );
+        System.out.println("Berapa Umur Pohon : " + umur + "Tahun" );
+        System.out.println("Berapa Panjang Pohon : " + PanjangPohon + "M");
+    }  
+}
